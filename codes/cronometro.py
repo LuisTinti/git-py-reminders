b7a57827e02518ececb06ea1db8b73a262c2185f
LuisTinti <luisfelpstinti268@gmail.com>
2026-09-19 18:54:22 -0300

    sla

--- codes/cronometro.py
+++ codes/cronometro.py
@@ -6,6 +6,7 @@ class Cronometro:
         self.root.title("Cronômetro")
         self.root.geometry("300x200")
         self.root.configure(bg="#000408")
+        self.root.resizable(True, True)
         
         # Variáveis de controle de tempo
         self.tempo_decorrido = 0
